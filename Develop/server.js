b6ff4bf87dcee3e9f52090063d67ebcd7ead3609
yomi odusi <yomiodusi98@gmail.com>
2022-06-19 21:51:13 -0400

    config and server page complete

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+const sess = {
+  secret: 'on the low secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
-ddd
